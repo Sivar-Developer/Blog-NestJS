6a8e3a45609569fe88f1c545006f84dc34d992d6
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-10-16 15:15:59 +0300

    User module CRUD Operations

---
+++ api/src/user/controller/user.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+import { UserService } from '../service/user.service';
+
+@Controller('users')
+export class UserController {
+
+    constructor(private userService: UserService) {}
+
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user)
+    }
+
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param() params): Observable<User> {
+        return this.userService.findOne(params.id)
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: string): Observable<User> {
+        return this.userService.deleteOne(Number(id));
+    }
+
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
+        return this.userService.updateOne(Number(id), user);
+    }
+}
