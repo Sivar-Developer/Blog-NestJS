df0309997fe8e4e6a1f4db4039b939db6b3b3ae6
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-10-18 23:58:33 +0300

    Blog Delete function

--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post, Query, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Request, UseGuards } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
+import { UserIsAuthorGuard } from '../guards/user-is-author.guard';
 import { Blog } from '../model/blog.interface';
 import { BlogService } from '../service/blog.service';
 
@@ -28,6 +29,18 @@ export class BlogController {
     store(@Body() blog: Blog, @Request() req): Observable<Blog> {
         const user = req.user
         return this.blogService.create(user, blog)
+    }
+
+    @UseGuards(JwtAuthGuard, UserIsAuthorGuard)
+    @Put(':id')
+    update(@Param('id') id: number, @Body() blog: Blog): Observable<Blog> {
+        return this.blogService.update(Number(id), blog)
+    }
+
+    @UseGuards(JwtAuthGuard, UserIsAuthorGuard)
+    @Delete(':id')
+    delete(@Param('id') id: number): Observable<any> {
+        return this.blogService.delete(Number(id))
     }
 
 }
